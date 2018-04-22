d919e24635ff6dc9005807a2d64a6c5c11fcf7f7
pdomala2 <pdomala2@csc.com>
2018-04-22 22:12:20 +1000

    V2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V1' });
+  res.render('index', { title: 'Express V2' });
 });
 
 module.exports = router;
