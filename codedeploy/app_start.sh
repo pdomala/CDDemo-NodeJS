e891ba6abf7ac17a5e790fafbcfd7ae2a89351ff
pdomala2 <pdomala2@csc.com>
2018-04-22 20:31:36 +1000

    debug

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -2,4 +2,4 @@
 
 # This script is used to start the application
 cd /usr/cddemo
-pm2 start /usr/cddemo/bin/www -n www
+pm2 start /usr/cddemo/bin/www -n www -f
--- codedeploy/app_stop.sh
+++ codedeploy/app_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This script is used to stop application
-cd $app_root
+cd cd /usr/cddemo
 pm2 stop www || true
